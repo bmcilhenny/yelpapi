544cde629981599c85593ac9545c8bd8a6e3a15b
bmcilhenny <bmcilhen@gmail.com>
2018-05-04 22:42:46 -0400

    change background image

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>ravenous</h1>
+        <h1>Grumble</h1>
       <SearchBar searchYelp={this.searchYelp}/>
     <BusinessList businesses={this.state.businesses}/>
       </div>
